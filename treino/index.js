67b1bf3cd1f8638d1aa7406c44bb8a0e6ff18616
Gabriel <gabrielldarts@gmail.com>
2021-01-02 18:47:11 -0300

    training automatic save

--- treino/index.js
+++ treino/index.js
@@ -14,8 +14,41 @@
         remadaCurvada: 0,
         pullover: 0,
       },
-      treinoB: {},
-      treinoC: {},
+      treinoB: {
+        roscaDireta: 0,
+        tricepsTesta: 0,
+        roscaAlternada: 0,
+        tricepsPulley: 0,
+        roscaMartelo: 0,
+        tricepsFrancesUnilateral: 0,
+        roscaScott: 0,
+        TricepsPulleyInverso: 0,
+        extensorPunho: 0,
+        extensorPunhoInverso: 0,
+      },
+      treinoC: {
+        desenvolvimentoFrente: 0,
+        desenvolvimentoComHalter: 0,
+        elevaçãoLateral: 0,
+        elevaçãoFrontal: 0,
+        cruxifixoInverso: 0,
+        elevaçãoFrontalBarra: 0,
+        encolhimento: 0,
+        abdomenSupra: 0,
+        abdomenInfra: 0,
+        abdomenRemador: 0,
+      },
+      treinoD: {
+        agachamento: 0,
+        legPress: 0,
+        extensor: 0,
+        flexor: 0,
+        adutor: 0,
+        abdutor: 0,
+        agachamentoHack: 0,
+        panturrilhaEmPe: 0,
+        panturrilhaSentado: 0,
+      },
     };
     window.localStorage.setItem('@memory', JSON.stringify(training));
   }
@@ -24,82 +57,99 @@
   const treinoA = document.querySelector('div.treino-a');
   const treinoB = document.querySelector('div.treino-b');
   const treinoC = document.querySelector('div.treino-c');
+  const treinoD = document.querySelector('div.treino-d');
 
   const memory = JSON.parse(localStorage.getItem('@memory'));
 
-  const contentTreinoA = `
-    <div class="biset">
+  function insertElement(section, treinoSeparado) {
+    Object.keys(treinoSeparado).forEach((exercicio) => {
+      const divExercicio = `
         <div class="flex-area j-space-between linha">
-        <p>Supino Reto</p>
-        <input type="text" value="${memory.treinoA.supinoReto}" id="input-supino-reto">
-    </div>
-    <div class="flex-area j-space-between linha">
-        <p>Puxada Frente</p>
-        <input type="text" value="${memory.treinoA.puxadaFrente}" id="input-puxada-frente">
-      </div>
-    </div>
+          <p>${exercicio.replace(/([A-Z])/g, ' $1')}</p>
+          <input type="text" value="${
+            treinoSeparado[exercicio]
+          }" id="${exercicio}">
+        </div>
+      `;
+      section.insertAdjacentHTML('beforeend', divExercicio);
+    });
+  }
 
-    <div class="biset">
-      <div class="flex-area j-space-between linha">
-          <p>Supino Inclinado</p>
-          <input type="text" value="${memory.treinoA.supinoInclinado}" id="input-supino-inclinado">
-      </div>
-      <div class="flex-area j-space-between linha">
-          <p>Puxada Triangulo</p>
-          <input type="text" value="${memory.treinoA.puxadaTriangulo}" id="input-puxada-triangulo">
-      </div>
-    </div>
-
-    <div class="biset">
-      <div class="flex-area j-space-between linha">
-          <p>Crucifixo Reto</p>
-          <input type="text" value="${memory.treinoA.crucifixoReto}" id="input-crucifixo-reto">
-      </div>
-      <div class="flex-area j-space-between linha">
-          <p>Remada Baixa</p>
-          <input type="text" value="${memory.treinoA.remadaBaixa}" id="input-remada-baixa">
-      </div>
-    </div>
-
-    <div class="biset">
-      <div class="flex-area j-space-between linha">
-          <p>Cross Over</p>
-          <input type="text" value="${memory.treinoA.crossOver}" id="input-cross-over">
-      </div>
-      <div class="flex-area j-space-between linha">
-          <p>Remada Curvada</p>
-          <input type="text" value="${memory.treinoA.remadaCurvada}" id="input-remada-curvada">
-      </div>
-    </div>
-
-    <div class="flex-area j-space-between linha">
-      <p>Pullover</p>
-      <input type="text" value="${memory.treinoA.pullover}" id="input-pullover">
-    </div>
-  `;
-
-  treinoA.insertAdjacentHTML('beforeend', contentTreinoA);
+  Object.keys(memory).forEach((treino) => {
+    const treinoSeparado = memory[treino];
+    if (treino === 'treinoA') {
+      insertElement(treinoA, treinoSeparado);
+    } else if (treino === 'treinoB') {
+      insertElement(treinoB, treinoSeparado);
+    } else if (treino === 'treinoC') {
+      insertElement(treinoC, treinoSeparado);
+    } else if (treino === 'treinoD') {
+      insertElement(treinoD, treinoSeparado);
+    }
+  });
 
   document.querySelector('button').addEventListener('click', saveMemory);
+
   function saveMemory(event) {
     event.preventDefault();
+
     const trainingNew = {
       treinoA: {
-        supinoReto: document.querySelector('#input-supino-reto').value,
-        puxadaFrente: document.querySelector('#input-puxada-frente').value,
-        supinoInclinado: document.querySelector('#input-supino-inclinado')
-          .value,
-        puxadaTriangulo: document.querySelector('#input-puxada-triangulo')
-          .value,
-        crucifixoReto: document.querySelector('#input-crucifixo-reto').value,
-        remadaBaixa: document.querySelector('#input-remada-baixa').value,
-        crossOver: document.querySelector('#input-cross-over').value,
-        remadaCurvada: document.querySelector('#input-remada-curvada').value,
-        pullover: document.querySelector('#input-pullover').value,
+        supinoReto: document.querySelector('#supinoReto').value,
+        puxadaFrente: document.querySelector('#puxadaFrente').value,
+        supinoInclinado: document.querySelector('#supinoInclinado').value,
+        puxadaTriangulo: document.querySelector('#puxadaTriangulo').value,
+        crucifixoReto: document.querySelector('#crucifixoReto').value,
+        remadaBaixa: document.querySelector('#remadaBaixa').value,
+        crossOver: document.querySelector('#crossOver').value,
+        remadaCurvada: document.querySelector('#remadaCurvada').value,
+        pullover: document.querySelector('#pullover').value,
       },
-      treinoB: {},
-      treinoC: {},
+      treinoB: {
+        roscaDireta: document.querySelector('#roscaDireta').value,
+        tricepsTesta: document.querySelector('#tricepsTesta').value,
+        roscaAlternada: document.querySelector('#roscaAlternada').value,
+        tricepsPulley: document.querySelector('#tricepsPulley').value,
+        roscaMartelo: document.querySelector('#roscaMartelo').value,
+        tricepsFrancesUnilateral: document.querySelector(
+          '#tricepsFrancesUnilateral',
+        ).value,
+        roscaScott: document.querySelector('#roscaScott').value,
+        TricepsPulleyInverso: document.querySelector('#TricepsPulleyInverso')
+          .value,
+        extensorPunho: document.querySelector('#extensorPunho').value,
+        extensorPunhoInverso: document.querySelector('#extensorPunhoInverso')
+          .value,
+      },
+      treinoC: {
+        desenvolvimentoFrente: document.querySelector('#desenvolvimentoFrente')
+          .value,
+        desenvolvimentoComHalter: document.querySelector(
+          '#desenvolvimentoComHalter',
+        ).value,
+        elevaçãoLateral: document.querySelector('#elevaçãoLateral').value,
+        elevaçãoFrontal: document.querySelector('#elevaçãoFrontal').value,
+        cruxifixoInverso: document.querySelector('#cruxifixoInverso').value,
+        elevaçãoFrontalBarra: document.querySelector('#elevaçãoFrontalBarra')
+          .value,
+        encolhimento: document.querySelector('#encolhimento').value,
+        abdomenSupra: document.querySelector('#abdomenSupra').value,
+        abdomenInfra: document.querySelector('#abdomenInfra').value,
+        abdomenRemador: document.querySelector('#abdomenRemador').value,
+      },
+      treinoD: {
+        agachamento: document.querySelector('#agachamento').value,
+        legPress: document.querySelector('#legPress').value,
+        extensor: document.querySelector('#extensor').value,
+        flexor: document.querySelector('#flexor').value,
+        adutor: document.querySelector('#adutor').value,
+        abdutor: document.querySelector('#abdutor').value,
+        agachamentoHack: document.querySelector('#agachamentoHack').value,
+        panturrilhaEmPe: document.querySelector('#panturrilhaEmPe').value,
+        panturrilhaSentado: document.querySelector('#panturrilhaSentado').value,
+      },
     };
+
     window.localStorage.setItem('@memory', JSON.stringify(trainingNew));
   }
 })();
